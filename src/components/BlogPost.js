05e2ad26a7407854432b066effaa4c4b6eda311b
LUCY <wangecikariu.luc@gmail.com>
2023-08-27 13:16:55 +0300

    Props

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,9 +2,10 @@ import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
+
 function BlogPost() {
   return (
-    <div id="blog-post">
+    <div id="blogPost">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
